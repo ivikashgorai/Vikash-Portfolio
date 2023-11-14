1a6bbb68bcac9913b3a29252cddd02cf1a957ada
ivikashgorai <vikashgorai985276@gmail.com>
2023-11-15 00:05:41 +0530

    complete darkmode

--- Scripting/script.js
+++ Scripting/script.js
@@ -3,7 +3,10 @@ const darkmode = document.querySelector(".dark-mode");
 const color = document.querySelectorAll(".col");
 const projectOneView = document.querySelector('.one-view');
 const projectOneCode = document.querySelector('.one-code');
-
+const techbg = document.querySelectorAll('.skill-item-tech');
+const contactbg = document.querySelectorAll('.contact-link');
+const projectWork  = document.querySelectorAll('.project-work');
+const tech  = document.querySelectorAll('.tech');
 //NavBar
 let a = 1;
 button.addEventListener("click", () => {
@@ -28,16 +31,40 @@ let dark = 1;
 darkmode.addEventListener("click", () => {
   if (dark == 1) {
     darkmode.innerHTML = `<i class="fa-solid fa-lightbulb" style="color:white"></i>`;
-    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.backgroundColor = "#03001C";
     for (let i = 0; i < color.length; i++) {
-      color[i].style.color = "white";
+      color[i].style.color = "grey";
+    }
+    for (let j = 0; j < techbg.length; j++) {
+      techbg[j].style.backgroundColor = "#301E67";
+    }
+    for (let j = 0; j < contactbg.length; j++) {
+      contactbg[j].style.backgroundColor = "#301E67";
+    }
+    for (let j = 0; j < projectWork.length; j++) {
+      projectWork[j].style.borderColor = "#301E67";
+    }
+    for (let j = 0; j < tech.length; j++) {
+      tech[j].style.borderColor = "#301E67";
     }
     dark = 0;
   } else {
     darkmode.innerHTML = `<i class="fa-solid fa-cloud-moon"></i>`;
     document.querySelector("body").style.backgroundColor = "white";
     for (let i = 0; i < color.length; i++) {
-      color[i].style.color = "black";
+      color[i].style.color = "rgb(53,48,98)";
+    }
+    for (let j = 0; j < techbg.length; j++) {
+      techbg[j].style.backgroundColor = "rgb(234, 240, 238)";
+    }
+    for (let j = 0; j < contactbg.length; j++) {
+      contactbg[j].style.backgroundColor = "rgb(234, 240, 238)";
+    }
+    for (let j = 0; j < projectWork.length; j++) {
+      projectWork[j].style.borderColor = "rgb(220, 220, 220)";
+    }
+    for (let j = 0; j < tech.length; j++) {
+      tech[j].style.borderColor = "#301E67";
     }
     dark = 1;
   }
